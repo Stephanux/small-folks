22906f9d78a72dce48270140dca97f7697372e6f
stephane <stephane@mascaron.net>
2026-03-24 16:22:31 +0100

    Ajout d'un test de connexion à la base NoSQL MongoDB

---
+++ mongodb_test/src/main.rs
@@ -0,0 +1,32 @@
+use mongodb::{ 
+    Client, Collection, bson::{doc, oid::ObjectId}, options::{ClientOptions, Credential}
+};
+use serde::{ Deserialize, Serialize };
+#[derive(Serialize, Deserialize, Debug)]
+struct Countries {
+    _id: ObjectId,
+    name: String,
+    code: String,
+}
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "mongodb://127.0.0.1:27017/r310";
+    let mut client_options = ClientOptions::parse(uri).await?;
+    let default_cred = Credential::builder()
+    .username("admin".to_string())
+    .password("azerty".to_string())
+    .source("admin".to_string())
+    .build();
+    client_options.credential = Some(default_cred);
+    let client = Client::with_options(client_options)?;
+    // Replace <T> with the <Document> or <Restaurant> type parameter
+    let my_coll: Collection<Countries> = client
+        .database("r310")
+        .collection("countries");
+    let mut result = my_coll.find(doc! {}).await?;
+    println!("{:#?}", result);
+    while result.advance().await? {
+        println!("{:?}", result.deserialize_current()?);
+    }
+    Ok(())
+}
